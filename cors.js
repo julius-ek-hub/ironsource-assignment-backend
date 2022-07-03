c1cc09d4e9c23d4948c439f818f0acc6bd07c542
julius_ek_hub <ekanejulius7@gmail.com>
2022-07-03 09:23:34 +0400

    Edited cors to allowl local origin in production as well

--- cors.js
+++ cors.js
@@ -1,10 +1,6 @@
-const env = (key) => process.env[key];
-
 module.exports = (req, res, next) => {
-	["ALLOWED_ORIGIN_production", "ALLOWED_ORIGIN_development"].map((Origin) => {
-		res.setHeader("Access-Control-Allow-Origin", env(Origin));
-	});
-	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+	res.setHeader("Access-Control-Allow-Origin", "*");
 	res.setHeader("Access-Control-Allow-Methods", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 	next();
 };
