2a52539dc131a013e0e3c4784826764f752ec587
julius_ek_hub <ekanejulius7@gmail.com>
2022-07-03 12:37:47 +0400

    Fixed typo in db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const env = (key) => process.env[key];
 module.exports = (req, res, next) => {
 	if (mongoose.connection.readyState !== 0) return next();
 
-	const CONECTION_STRING = env(`DB_CONNECTION_STRING_production`);
+	const CONECTION_STRING = env(`DB_CONNECTION_STRING_${$env("NODE_ENV")}`);
 
 	mongoose.connect(CONECTION_STRING, null, (err) => {
 		if (err) return res.status(500).end("Could not connect to database");
